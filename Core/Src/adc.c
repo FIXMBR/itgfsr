62f25151313a48b61a55d1e18fbbc9d11efb7bc1
FIXMBR <16748784+FIXMBR@users.noreply.github.com>
2022-12-23 22:03:31 +0100

    kupsko2

--- Core/Src/adc.c
+++ Core/Src/adc.c
@@ -25,7 +25,6 @@
 /* USER CODE END 0 */
 
 ADC_HandleTypeDef hadc1;
-ADC_HandleTypeDef hadc2;
 DMA_HandleTypeDef hdma_adc1;
 
 /* ADC1 init function */
@@ -36,7 +35,6 @@ void MX_ADC1_Init(void)
 
   /* USER CODE END ADC1_Init 0 */
 
-  ADC_MultiModeTypeDef multimode = {0};
   ADC_ChannelConfTypeDef sConfig = {0};
 
   /* USER CODE BEGIN ADC1_Init 1 */
@@ -51,16 +49,8 @@ void MX_ADC1_Init(void)
   hadc1.Init.DiscontinuousConvMode = DISABLE;
   hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
   hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
-  hadc1.Init.NbrOfConversion = 5;
+  hadc1.Init.NbrOfConversion = 10;
   if (HAL_ADC_Init(&hadc1) != HAL_OK)
-  {
-    Error_Handler();
-  }
-
-  /** Configure the ADC multi-mode
-  */
-  multimode.Mode = ADC_DUALMODE_REGSIMULT;
-  if (HAL_ADCEx_MultiModeConfigChannel(&hadc1, &multimode) != HAL_OK)
   {
     Error_Handler();
   }
@@ -77,6 +67,7 @@ void MX_ADC1_Init(void)
 
   /** Configure Regular Channel
   */
+  sConfig.Channel = ADC_CHANNEL_1;
   sConfig.Rank = ADC_REGULAR_RANK_2;
   if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
   {
@@ -85,6 +76,7 @@ void MX_ADC1_Init(void)
 
   /** Configure Regular Channel
   */
+  sConfig.Channel = ADC_CHANNEL_2;
   sConfig.Rank = ADC_REGULAR_RANK_3;
   if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
   {
@@ -93,6 +85,7 @@ void MX_ADC1_Init(void)
 
   /** Configure Regular Channel
   */
+  sConfig.Channel = ADC_CHANNEL_3;
   sConfig.Rank = ADC_REGULAR_RANK_4;
   if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
   {
@@ -101,40 +94,18 @@ void MX_ADC1_Init(void)
 
   /** Configure Regular Channel
   */
+  sConfig.Channel = ADC_CHANNEL_4;
   sConfig.Rank = ADC_REGULAR_RANK_5;
   if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
   {
     Error_Handler();
   }
-  /* USER CODE BEGIN ADC1_Init 2 */
 
-  /* USER CODE END ADC1_Init 2 */
-
-}
-/* ADC2 init function */
-void MX_ADC2_Init(void)
-{
-
-  /* USER CODE BEGIN ADC2_Init 0 */
-
-  /* USER CODE END ADC2_Init 0 */
-
-  ADC_ChannelConfTypeDef sConfig = {0};
-
-  /* USER CODE BEGIN ADC2_Init 1 */
-
-  /* USER CODE END ADC2_Init 1 */
-
-  /** Common config
+  /** Configure Regular Channel
   */
-  hadc2.Instance = ADC2;
-  hadc2.Init.ScanConvMode = ADC_SCAN_ENABLE;
-  hadc2.Init.ContinuousConvMode = DISABLE;
-  hadc2.Init.DiscontinuousConvMode = DISABLE;
-  hadc2.Init.ExternalTrigConv = ADC_SOFTWARE_START;
-  hadc2.Init.DataAlign = ADC_DATAALIGN_RIGHT;
-  hadc2.Init.NbrOfConversion = 5;
-  if (HAL_ADC_Init(&hadc2) != HAL_OK)
+  sConfig.Channel = ADC_CHANNEL_5;
+  sConfig.Rank = ADC_REGULAR_RANK_6;
+  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
   {
     Error_Handler();
   }
@@ -142,47 +113,41 @@ void MX_ADC2_Init(void)
   /** Configure Regular Channel
   */
   sConfig.Channel = ADC_CHANNEL_6;
-  sConfig.Rank = ADC_REGULAR_RANK_1;
-  sConfig.SamplingTime = ADC_SAMPLETIME_1CYCLE_5;
-  if (HAL_ADC_ConfigChannel(&hadc2, &sConfig) != HAL_OK)
+  sConfig.Rank = ADC_REGULAR_RANK_7;
+  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
   {
     Error_Handler();
   }
 
   /** Configure Regular Channel
   */
-  sConfig.Rank = ADC_REGULAR_RANK_2;
-  if (HAL_ADC_ConfigChannel(&hadc2, &sConfig) != HAL_OK)
+  sConfig.Channel = ADC_CHANNEL_7;
+  sConfig.Rank = ADC_REGULAR_RANK_8;
+  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
   {
     Error_Handler();
   }
 
   /** Configure Regular Channel
   */
-  sConfig.Rank = ADC_REGULAR_RANK_3;
-  if (HAL_ADC_ConfigChannel(&hadc2, &sConfig) != HAL_OK)
+  sConfig.Channel = ADC_CHANNEL_8;
+  sConfig.Rank = ADC_REGULAR_RANK_9;
+  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
   {
     Error_Handler();
   }
 
   /** Configure Regular Channel
   */
-  sConfig.Rank = ADC_REGULAR_RANK_4;
-  if (HAL_ADC_ConfigChannel(&hadc2, &sConfig) != HAL_OK)
+  sConfig.Channel = ADC_CHANNEL_9;
+  sConfig.Rank = ADC_REGULAR_RANK_10;
+  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
   {
     Error_Handler();
   }
+  /* USER CODE BEGIN ADC1_Init 2 */
 
-  /** Configure Regular Channel
-  */
-  sConfig.Rank = ADC_REGULAR_RANK_5;
-  if (HAL_ADC_ConfigChannel(&hadc2, &sConfig) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN ADC2_Init 2 */
-
-  /* USER CODE END ADC2_Init 2 */
+  /* USER CODE END ADC1_Init 2 */
 
 }
 
@@ -199,17 +164,27 @@ void HAL_ADC_MspInit(ADC_HandleTypeDef* adcHandle)
     __HAL_RCC_ADC1_CLK_ENABLE();
 
     __HAL_RCC_GPIOA_CLK_ENABLE();
+    __HAL_RCC_GPIOB_CLK_ENABLE();
     /**ADC1 GPIO Configuration
     PA0-WKUP     ------> ADC1_IN0
     PA1     ------> ADC1_IN1
     PA2     ------> ADC1_IN2
     PA3     ------> ADC1_IN3
     PA4     ------> ADC1_IN4
+    PA5     ------> ADC1_IN5
+    PA6     ------> ADC1_IN6
+    PA7     ------> ADC1_IN7
+    PB0     ------> ADC1_IN8
+    PB1     ------> ADC1_IN9
     */
     GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3
-                          |GPIO_PIN_4;
+                          |GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7;
     GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
     HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+    GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1;
+    GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
     /* ADC1 DMA Init */
     /* ADC1 Init */
@@ -235,38 +210,6 @@ void HAL_ADC_MspInit(ADC_HandleTypeDef* adcHandle)
 
   /* USER CODE END ADC1_MspInit 1 */
   }
-  else if(adcHandle->Instance==ADC2)
-  {
-  /* USER CODE BEGIN ADC2_MspInit 0 */
-
-  /* USER CODE END ADC2_MspInit 0 */
-    /* ADC2 clock enable */
-    __HAL_RCC_ADC2_CLK_ENABLE();
-
-    __HAL_RCC_GPIOA_CLK_ENABLE();
-    __HAL_RCC_GPIOB_CLK_ENABLE();
-    /**ADC2 GPIO Configuration
-    PA5     ------> ADC2_IN5
-    PA6     ------> ADC2_IN6
-    PA7     ------> ADC2_IN7
-    PB0     ------> ADC2_IN8
-    PB1     ------> ADC2_IN9
-    */
-    GPIO_InitStruct.Pin = GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7;
-    GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
-    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-
-    GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1;
-    GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
-    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-
-    /* ADC2 interrupt Init */
-    HAL_NVIC_SetPriority(ADC1_2_IRQn, 0, 0);
-    HAL_NVIC_EnableIRQ(ADC1_2_IRQn);
-  /* USER CODE BEGIN ADC2_MspInit 1 */
-
-  /* USER CODE END ADC2_MspInit 1 */
-  }
 }
 
 void HAL_ADC_MspDeInit(ADC_HandleTypeDef* adcHandle)
@@ -286,57 +229,25 @@ void HAL_ADC_MspDeInit(ADC_HandleTypeDef* adcHandle)
     PA2     ------> ADC1_IN2
     PA3     ------> ADC1_IN3
     PA4     ------> ADC1_IN4
+    PA5     ------> ADC1_IN5
+    PA6     ------> ADC1_IN6
+    PA7     ------> ADC1_IN7
+    PB0     ------> ADC1_IN8
+    PB1     ------> ADC1_IN9
     */
     HAL_GPIO_DeInit(GPIOA, GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3
-                          |GPIO_PIN_4);
+                          |GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7);
+
+    HAL_GPIO_DeInit(GPIOB, GPIO_PIN_0|GPIO_PIN_1);
 
     /* ADC1 DMA DeInit */
     HAL_DMA_DeInit(adcHandle->DMA_Handle);
 
     /* ADC1 interrupt Deinit */
-  /* USER CODE BEGIN ADC1:ADC1_2_IRQn disable */
-    /**
-    * Uncomment the line below to disable the "ADC1_2_IRQn" interrupt
-    * Be aware, disabling shared interrupt may affect other IPs
-    */
-    /* HAL_NVIC_DisableIRQ(ADC1_2_IRQn); */
-  /* USER CODE END ADC1:ADC1_2_IRQn disable */
-
+    HAL_NVIC_DisableIRQ(ADC1_2_IRQn);
   /* USER CODE BEGIN ADC1_MspDeInit 1 */
 
   /* USER CODE END ADC1_MspDeInit 1 */
-  }
-  else if(adcHandle->Instance==ADC2)
-  {
-  /* USER CODE BEGIN ADC2_MspDeInit 0 */
-
-  /* USER CODE END ADC2_MspDeInit 0 */
-    /* Peripheral clock disable */
-    __HAL_RCC_ADC2_CLK_DISABLE();
-
-    /**ADC2 GPIO Configuration
-    PA5     ------> ADC2_IN5
-    PA6     ------> ADC2_IN6
-    PA7     ------> ADC2_IN7
-    PB0     ------> ADC2_IN8
-    PB1     ------> ADC2_IN9
-    */
-    HAL_GPIO_DeInit(GPIOA, GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7);
-
-    HAL_GPIO_DeInit(GPIOB, GPIO_PIN_0|GPIO_PIN_1);
-
-    /* ADC2 interrupt Deinit */
-  /* USER CODE BEGIN ADC2:ADC1_2_IRQn disable */
-    /**
-    * Uncomment the line below to disable the "ADC1_2_IRQn" interrupt
-    * Be aware, disabling shared interrupt may affect other IPs
-    */
-    /* HAL_NVIC_DisableIRQ(ADC1_2_IRQn); */
-  /* USER CODE END ADC2:ADC1_2_IRQn disable */
-
-  /* USER CODE BEGIN ADC2_MspDeInit 1 */
-
-  /* USER CODE END ADC2_MspDeInit 1 */
   }
 }
 
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -72,7 +72,7 @@ typedef struct
 } subKeyBoard;
 
 // subKeyBoard keyBoardHIDsub = {0,0,0,0,0,0,0,0};//,0,0,0};
-uint8_t volatile keyBoardHIDsub[8] = { 0, 0, 0, 0, 0, 0, 0, 0};//, 0, 0 }; //,0,0,0};
+uint8_t volatile keyBoardHIDsub[8] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,0x00, 0x00};//, 0, 0 }; //,0,0,0};
 // subKeyBoard keyBoardHIDsub = {0,0,0,0,0,0,0,0,0,0,0};
 
 uint16_t  adc_results_1[10];
@@ -82,7 +82,7 @@ uint16_t  adc_results_4[10];
 uint8_t volatile ground_id=1;
 
 uint16_t sensor_treshholds[40]={
-		2750,2750,2750,2750,2750,2750,2750,2750,2750,2750,
+		3000,2750,2750,2750,2750,2750,2750,2750,2750,2750,
 		2750,2750,2750,2750,2750,2750,2750,2750,2750,2750,
 		2750,2750,2750,2750,2750,2750,2750,2750,2750,2750,
 		2750,2750,2750,2750,2750,2750,2750,2750,2750,2750
@@ -90,6 +90,7 @@ uint16_t sensor_treshholds[40]={
 
 uint16_t raw;
 char msg[10];
+uint16_t volatile debug_var=0;
 
 //uint8_t sensors_groups[40]={
 //		0,1,2,3,4,5,6,7,8,9,
@@ -106,50 +107,55 @@ uint8_t sensors_keymap[10]={
 		KEY_Z,KEY_X,KEY_C,
 		KEY_1};
 
-static GPIO_InitTypeDef Output_1_in={
-		.Pin = Output_1_Pin,
-		.Mode = GPIO_MODE_INPUT,
-		.Pull = GPIO_NOPULL,
-};
 
-static GPIO_InitTypeDef Output_1_out={
-		.Pin = Output_1_Pin,
-		.Mode = GPIO_MODE_OUTPUT_PP,
-		.Pull = GPIO_NOPULL,
-};
-static GPIO_InitTypeDef Output_2_in={
-		.Pin = Output_2_Pin,
-		.Mode = GPIO_MODE_INPUT,
-		.Pull = GPIO_NOPULL,
-};
+static GPIO_InitTypeDef Output_1_in;
+static GPIO_InitTypeDef Output_1_out;
+static GPIO_InitTypeDef Output_2_in ;
+static GPIO_InitTypeDef Output_2_out;
+static GPIO_InitTypeDef Output_3_in;
+static GPIO_InitTypeDef Output_3_out;
+static GPIO_InitTypeDef Output_4_in ;
+static GPIO_InitTypeDef Output_4_out;
 
-static GPIO_InitTypeDef Output_2_out={
-		.Pin = Output_2_Pin,
-		.Mode = GPIO_MODE_OUTPUT_PP,
-		.Pull = GPIO_NOPULL,
-};
-static GPIO_InitTypeDef Output_3_in={
-		.Pin = Output_3_Pin,
-		.Mode = GPIO_MODE_INPUT,
-		.Pull = GPIO_NOPULL,
-};
 
-static GPIO_InitTypeDef Output_3_out={
-		.Pin = Output_3_Pin,
-		.Mode = GPIO_MODE_OUTPUT_PP,
-		.Pull = GPIO_NOPULL,
-};
-static GPIO_InitTypeDef Output_4_in={
-		.Pin = Output_4_Pin,
-		.Mode = GPIO_MODE_INPUT,
-		.Pull = GPIO_NOPULL,
-};
-
-static GPIO_InitTypeDef Output_4_out={
-		.Pin = Output_4_Pin,
-		.Mode = GPIO_MODE_OUTPUT_PP,
-		.Pull = GPIO_NOPULL,
-};
+//static GPIO_InitTypeDef Output_1_out={
+//		.Pin = Output_1_Pin,
+//		.Mode = GPIO_MODE_OUTPUT_PP,
+//		.Pull = GPIO_NOPULL,
+//};
+//static GPIO_InitTypeDef Output_2_in={
+//		.Pin = Output_2_Pin,
+//		.Mode = GPIO_MODE_INPUT,
+//		.Pull = GPIO_NOPULL,
+//};
+//
+//static GPIO_InitTypeDef Output_2_out={
+//		.Pin = Output_2_Pin,
+//		.Mode = GPIO_MODE_OUTPUT_PP,
+//		.Pull = GPIO_NOPULL,
+//};
+//static GPIO_InitTypeDef Output_3_in={
+//		.Pin = Output_3_Pin,
+//		.Mode = GPIO_MODE_INPUT,
+//		.Pull = GPIO_NOPULL,
+//};
+//
+//static GPIO_InitTypeDef Output_3_out={
+//		.Pin = Output_3_Pin,
+//		.Mode = GPIO_MODE_OUTPUT_PP,
+//		.Pull = GPIO_NOPULL,
+//};
+//static GPIO_InitTypeDef Output_4_in={
+//		.Pin = Output_4_Pin,
+//		.Mode = GPIO_MODE_INPUT,
+//		.Pull = GPIO_NOPULL,
+//};
+//
+//static GPIO_InitTypeDef Output_4_out={
+//		.Pin = Output_4_Pin,
+//		.Mode = GPIO_MODE_OUTPUT_PP,
+//		.Pull = GPIO_NOPULL,
+//};
 
 //static GPIO_InitTypeDef Output_1_in;
 //Output_1_in.Pin;
@@ -173,34 +179,36 @@ void SystemClock_Config(void);
 /* USER CODE BEGIN 0 */
 void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc)
 {
-//	CDC_Transmit(0,"gowno", sizeof("gowno"));
-	ground_id=69;
-//	switch (ground_id) {
-//	case 1:
-//		HAL_GPIO_Init(Output_1_GPIO_Port, &Output_1_in);
-//		HAL_GPIO_Init(Output_2_GPIO_Port, &Output_2_out);
-//		ground_id=2;
-//		HAL_ADC_Start_DMA(&hadc1, &adc_results_2, 10);
-//		break;
-//	case 2:
-//		HAL_GPIO_Init(Output_2_GPIO_Port, &Output_2_in);
-//		HAL_GPIO_Init(Output_3_GPIO_Port, &Output_3_out);
-//		ground_id=3;
-//		HAL_ADC_Start_DMA(&hadc1, &adc_results_3, 10);
-//		break;
-//	case 3:
-//		HAL_GPIO_Init(Output_3_GPIO_Port, &Output_3_in);
-//		HAL_GPIO_Init(Output_4_GPIO_Port, &Output_4_out);
-//		ground_id=4;
-//		HAL_ADC_Start_DMA(&hadc1, &adc_results_4, 10);
-//		break;
-//	case 4:
-//		HAL_GPIO_Init(Output_4_GPIO_Port, &Output_4_in);
-//		HAL_GPIO_Init(Output_1_GPIO_Port, &Output_1_out);
-//		ground_id=1;
-//		HAL_ADC_Start_DMA(&hadc1, &adc_results_1, 10);
-//		break;
-//	}
+	//	CDC_Transmit(0,"gowno", sizeof("gowno"));
+	//	ground_id=69;
+	debug_var++;
+	HAL_ADC_Stop_DMA(&hadc1);
+	switch (ground_id) {
+	case 1:
+		HAL_GPIO_Init(Output_1_GPIO_Port, &Output_1_in);
+		HAL_GPIO_Init(Output_2_GPIO_Port, &Output_2_out);
+		ground_id=2;
+		HAL_ADC_Start_DMA(&hadc1, &adc_results_2, 10);
+		break;
+	case 2:
+		HAL_GPIO_Init(Output_2_GPIO_Port, &Output_2_in);
+		HAL_GPIO_Init(Output_3_GPIO_Port, &Output_3_out);
+		ground_id=3;
+		HAL_ADC_Start_DMA(&hadc1, &adc_results_3, 10);
+		break;
+	case 3:
+		HAL_GPIO_Init(Output_3_GPIO_Port, &Output_3_in);
+		HAL_GPIO_Init(Output_4_GPIO_Port, &Output_4_out);
+		ground_id=4;
+		HAL_ADC_Start_DMA(&hadc1, &adc_results_4, 10);
+		break;
+	case 4:
+		HAL_GPIO_Init(Output_4_GPIO_Port, &Output_4_in);
+		HAL_GPIO_Init(Output_1_GPIO_Port, &Output_1_out);
+		ground_id=1;
+		HAL_ADC_Start_DMA(&hadc1, &adc_results_1, 10);
+		break;
+	}
 }
 
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim){
@@ -210,102 +218,152 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim){
 			for (int j = 0; j < 4; ++j) {
 				switch (j) {
 				case 0:
-//					sprintf(msg, "%hu ", adc_results_1[j]);
-//					CDC_Transmit(0,msg, sizeof(msg));
-					if(sensor_treshholds[i+j*10]<adc_results_1[j]){
-						sensors_states[j]=1;
+					//					sprintf(msg, "%hu ", adc_results_1[j]);
+					//					CDC_Transmit(0,msg, sizeof(msg));
+					if(sensor_treshholds[i+j*10]>adc_results_1[i]){
+						sensors_states[i]=1;
+						//						sprintf(msg, "%hu ", adc_results_1[j]);
+						//											CDC_Transmit(0,msg, sizeof(msg));
 					}
 					break;
 				case 1:
-//					sprintf(msg, "%hu ", adc_results_2[j]);
-//					CDC_Transmit(0,msg, sizeof(msg));
-					if(sensor_treshholds[i+j*10]<adc_results_2[j]){
-						sensors_states[j]=1;
+					//					sprintf(msg, "%hu ", adc_results_2[j]);
+					//					CDC_Transmit(0,msg, sizeof(msg));
+					if(sensor_treshholds[i+j*10]>adc_results_2[i]){
+						sensors_states[i]=1;
+						//						sprintf(msg, "%hu ", adc_results_2[j]);
+						//											CDC_Transmit(0,msg, sizeof(msg));
 					}
 
 					break;
 				case 2:
-//					sprintf(msg, "%hu ", adc_results_3[j]);
-//					CDC_Transmit(0,msg, sizeof(msg));
-					if(sensor_treshholds[i+j*10]<adc_results_3[j]){
-						sensors_states[j]=1;
+					//					sprintf(msg, "%hu ", adc_results_3[j]);
+					//					CDC_Transmit(0,msg, sizeof(msg));
+					if(sensor_treshholds[i+j*10]>adc_results_3[i]){
+						sensors_states[i]=1;
+						//											sprintf(msg, "%hu ", adc_results_3[j]);
+						//											CDC_Transmit(0,msg, sizeof(msg));
 					}
 					break;
 				case 3:
-//					sprintf(msg, "%hu ", adc_results_4[j]);
-//					CDC_Transmit(0,msg, sizeof(msg));
-					if(sensor_treshholds[i+j*10]<adc_results_4[j]){
-						sensors_states[j]=1;
+					//					sprintf(msg, "%hu ", adc_results_4[j]);
+					//					CDC_Transmit(0,msg, sizeof(msg));
+					if(sensor_treshholds[i+j*10]>adc_results_4[i]){
+						sensors_states[i]=1;
+						//						sprintf(msg, "%hu ", adc_results_4[j]);
+						//											CDC_Transmit(0,msg, sizeof(msg));
 					}
 					break;
 				}
 
 			}
-//			CDC_Transmit(0,"kupka", sizeof("kupka"));
+			//			CDC_Transmit(0,"kupka", sizeof("kupka"));
 		}
 
-		for (int i = 0; i < 9; ++i) {
-			keyBoardHIDsub[i]=0x00;
-			if(sensors_states[i]==1){
-				for (int j = 2; j < 8; ++j) {
-					if(keyBoardHIDsub[j]==0x00){
-						keyBoardHIDsub[j]=sensors_keymap[i];
-					}
-				}
-			}
+		//		for (int i = 0; i < 9; ++i) {
+		//			keyBoardHIDsub[i]=0x00;
+		//			if(sensors_states[i]==1){
+		//				for (int j = 2; j < 8; ++j) {
+		//					if(keyBoardHIDsub[j]==0x00){
+		//						keyBoardHIDsub[j]=sensors_keymap[i];
+		//					}
+		//				}
+		//			}
+		//		}
+
+		if(sensors_states[0]==1){
+//			keyBoardHIDsub[2]=sensors_keymap[0];
+			keyBoardHIDsub[2]=0x05;
+		}else{
+			keyBoardHIDsub[2]=0x00;
 		}
 
-		USBD_HID_Keybaord_SendReport(&hUsbDevice, keyBoardHIDsub, sizeof(keyBoardHIDsub));
+
+		USBD_HID_Keybaord_SendReport(&hUsbDevice, &keyBoardHIDsub, sizeof(keyBoardHIDsub));
 	}
 }
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
+ * @brief  The application entry point.
+ * @retval int
+ */
 int main(void)
 {
-  /* USER CODE BEGIN 1 */
+	/* USER CODE BEGIN 1 */
 
 	int volatile jajco = 1;
-  /* USER CODE END 1 */
+	/* USER CODE END 1 */
 
-  /* MCU Configuration--------------------------------------------------------*/
+	/* MCU Configuration--------------------------------------------------------*/
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
+	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+	HAL_Init();
 
-  /* USER CODE BEGIN Init */
+	/* USER CODE BEGIN Init */
 
-  /* USER CODE END Init */
+	/* USER CODE END Init */
 
-  /* Configure the system clock */
-  SystemClock_Config();
+	/* Configure the system clock */
+	SystemClock_Config();
 
-  /* USER CODE BEGIN SysInit */
+	/* USER CODE BEGIN SysInit */
 
-  /* USER CODE END SysInit */
+	/* USER CODE END SysInit */
 
-  /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  MX_DMA_Init();
-  MX_ADC1_Init();
-  MX_ADC2_Init();
-  MX_USB_PCD_Init();
-  MX_TIM2_Init();
-  /* USER CODE BEGIN 2 */
+	/* Initialize all configured peripherals */
+	MX_GPIO_Init();
+	MX_DMA_Init();
+	MX_ADC1_Init();
+	MX_USB_PCD_Init();
+	MX_TIM2_Init();
+	/* USER CODE BEGIN 2 */
+	Output_1_in.Pin=Output_1_Pin;
+	Output_1_in.Mode=GPIO_MODE_INPUT;
+	Output_1_in.Pull=GPIO_NOPULL;
+	Output_1_in.Speed = GPIO_SPEED_FREQ_LOW;
+	Output_1_out.Pin=Output_1_Pin;
+	Output_1_out.Mode=GPIO_MODE_OUTPUT_PP;
+	Output_1_out.Pull=GPIO_NOPULL;
+	Output_1_out.Speed = GPIO_SPEED_FREQ_LOW;
+	Output_2_in.Pin=Output_2_Pin;
+	Output_2_in.Mode=GPIO_MODE_INPUT;
+	Output_2_in.Pull=GPIO_NOPULL;
+	Output_2_in.Speed = GPIO_SPEED_FREQ_LOW;
+	Output_2_out.Pin=Output_2_Pin;
+	Output_2_out.Mode=GPIO_MODE_OUTPUT_PP;
+	Output_2_out.Pull=GPIO_NOPULL;
+	Output_2_out.Speed = GPIO_SPEED_FREQ_LOW;
+	Output_3_in.Pin=Output_3_Pin;
+	Output_3_in.Mode=GPIO_MODE_INPUT;
+	Output_3_in.Pull=GPIO_NOPULL;
+	Output_3_in.Speed = GPIO_SPEED_FREQ_LOW;
+	Output_3_out.Pin=Output_3_Pin;
+	Output_3_out.Mode=GPIO_MODE_OUTPUT_PP;
+	Output_3_out.Pull=GPIO_NOPULL;
+	Output_3_out.Speed = GPIO_SPEED_FREQ_LOW;
+	Output_4_in.Pin=Output_4_Pin;
+	Output_4_in.Mode=GPIO_MODE_INPUT;
+	Output_4_in.Pull=GPIO_NOPULL;
+	Output_4_in.Speed = GPIO_SPEED_FREQ_LOW;
+	Output_4_out.Pin=Output_4_Pin;
+	Output_4_out.Mode=GPIO_MODE_OUTPUT_PP;
+	Output_4_out.Pull=GPIO_NOPULL;
+	Output_4_out.Speed = GPIO_SPEED_FREQ_LOW;
+
+
 	MX_USB_DEVICE_Init();
-	HAL_GPIO_Init(Output_1_GPIO_Port, &Output_1_out);
-	HAL_GPIO_Init(Output_2_GPIO_Port, &Output_2_in);
-	HAL_GPIO_Init(Output_3_GPIO_Port, &Output_3_in);
-	HAL_GPIO_Init(Output_4_GPIO_Port, &Output_4_in);
+	HAL_GPIO_Init(GPIOB, &Output_1_out);
+	HAL_GPIO_Init(GPIOB, &Output_2_out);
+	HAL_GPIO_Init(GPIOB, &Output_3_out);
+	HAL_GPIO_Init(GPIOB, &Output_4_out);
 	HAL_ADC_Start_DMA(&hadc1, &adc_results_1, 10);
 	HAL_TIM_Base_Start_IT(&htim2);
-  /* USER CODE END 2 */
+	//	HAL_ADCEx_Calibration_Start(&hadc1);
+	/* USER CODE END 2 */
 
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
+	/* Infinite loop */
+	/* USER CODE BEGIN WHILE */
 	while (1)
 	{
 		//		HAL_ADC_Start(&hadc1);
@@ -316,9 +374,10 @@ int main(void)
 		//		sprintf(msg, "%hu\r\n", raw);
 		//		//    		 CDC_Transmit(0, msg, sizeof(msg));
 		//		//    	     HAL_UART_Transmit(&huart2, (uint8_t*)msg, strlen(msg), HAL_MAX_DELAY);
-//		CDC_Transmit(0,"gowno", sizeof("gowno"));
-				sprintf(msg, "%d\r\n", ground_id);
-		CDC_Transmit(0,msg, sizeof(msg));
+		//		CDC_Transmit(0,"gowno", sizeof("gowno"));
+		//		sprintf(msg, "%d\r\n", ground_id);
+		//		sprintf(msg, "%d\r\n", debug_var);
+		//		CDC_Transmit(0,msg, sizeof(msg));
 		//		//    	     HAL_Delay(1);
 		//		//	     HAL_Delay(1);
 		//		if (raw < 2750)// && jajco == 1)
@@ -362,59 +421,104 @@ int main(void)
 		//			//			 keyBoardHIDsub.KEYCODE9=0x00;  // Release C key
 		//		}
 		//		// Pretend we have to do something else for a while
-				HAL_Delay(1);
-    /* USER CODE END WHILE */
 
-    /* USER CODE BEGIN 3 */
+
+
+
+
+		for (int i = 0; i < 10; ++i) {
+			sensors_states[i]=0;
+			for (int j = 0; j < 4; ++j) {
+				switch (j) {
+				case 0:
+					sprintf(msg, "%hu ", adc_results_1[i]);
+					CDC_Transmit(0,msg, sizeof(msg));
+					break;
+				case 1:
+					sprintf(msg, "%hu ", adc_results_2[i]);
+					CDC_Transmit(0,msg, sizeof(msg));
+					break;
+				case 2:
+					sprintf(msg, "%hu ", adc_results_3[i]);
+					CDC_Transmit(0,msg, sizeof(msg));
+					break;
+				case 3:
+					sprintf(msg, "%hu ", adc_results_4[i]);
+					CDC_Transmit(0,msg, sizeof(msg));
+					break;
+				}
+				HAL_Delay(1);
+			}
+
+			CDC_Transmit(0,"|\n\r", sizeof("|\n\r"));
+			HAL_Delay(1);
+		}
+
+		CDC_Transmit(0,"===\n\r", sizeof("===\n\r"));
+		HAL_Delay(1);
+		sprintf(msg, "%hu ", debug_var);
+		CDC_Transmit(0,msg, sizeof(msg));
+		debug_var=0;
+		HAL_Delay(1);
+		CDC_Transmit(0,"\n\r===\n\r", sizeof("\n\r===\n\r"));
+		HAL_Delay(1);
+		sprintf(msg, "%d ====", sensors_states[1]);
+		CDC_Transmit(0,msg, sizeof(msg));
+		HAL_Delay(1);
+		CDC_Transmit(0,"\n\r===\n\r", sizeof("\n\r===\n\r"));
+		HAL_Delay(500);
+		/* USER CODE END WHILE */
+
+		/* USER CODE BEGIN 3 */
 	}
-  /* USER CODE END 3 */
+	/* USER CODE END 3 */
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
+ * @brief System Clock Configuration
+ * @retval None
+ */
 void SystemClock_Config(void)
 {
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
-  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
+	RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+	RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+	RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
 
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-  RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL6;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
+	/** Initializes the RCC Oscillators according to the specified parameters
+	 * in the RCC_OscInitTypeDef structure.
+	 */
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+	RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+	RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+	RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+	RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL6;
+	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+	{
+		Error_Handler();
+	}
 
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+	/** Initializes the CPU, AHB and APB buses clocks
+	 */
+	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+			|RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_ADC|RCC_PERIPHCLK_USB;
-  PeriphClkInit.AdcClockSelection = RCC_ADCPCLK2_DIV4;
-  PeriphClkInit.UsbClockSelection = RCC_USBCLKSOURCE_PLL;
-  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
-  {
-    Error_Handler();
-  }
+	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
+	{
+		Error_Handler();
+	}
+	PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_ADC|RCC_PERIPHCLK_USB;
+	PeriphClkInit.AdcClockSelection = RCC_ADCPCLK2_DIV4;
+	PeriphClkInit.UsbClockSelection = RCC_USBCLKSOURCE_PLL;
+	if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+	{
+		Error_Handler();
+	}
 }
 
 /* USER CODE BEGIN 4 */
@@ -422,33 +526,33 @@ void SystemClock_Config(void)
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
 void Error_Handler(void)
 {
-  /* USER CODE BEGIN Error_Handler_Debug */
+	/* USER CODE BEGIN Error_Handler_Debug */
 	/* User can add his own implementation to report the HAL error return state */
 	__disable_irq();
 	while (1)
 	{
 	}
-  /* USER CODE END Error_Handler_Debug */
+	/* USER CODE END Error_Handler_Debug */
 }
 
 #ifdef  USE_FULL_ASSERT
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
 void assert_failed(uint8_t *file, uint32_t line)
 {
-  /* USER CODE BEGIN 6 */
+	/* USER CODE BEGIN 6 */
 	/* User can add his own implementation to report the file name and line number,
      ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
+	/* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
